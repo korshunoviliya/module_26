1a837cee32d3831aec1b8ea7aaa5477d7bbfb579
korshunoviliya <korshunov80@mail.ru>
2021-12-14 10:21:32 +0300

    Ввод пользователя перенесен в main()

--- module_26_2/main.cpp
+++ module_26_2/main.cpp
@@ -39,57 +39,41 @@ class Telephone {
 private:
     std::vector<Contact> contact;
 
-    int contactRequest() {
-        std::string tempInput;
-        std::cout << "Enter the name or number contact: ";
-        std::cin >> tempInput;
+    int contactRequest(std::string &tempInput) {
         if (Contact::validationNumber(tempInput)) {
             for (int i = 0; i < contact.size(); ++i) {
-                if (tempInput == contact[i].getNumber()) {
-                    return i;
-                }
-            }
-        } else {
-            for (int i = 0; i < contact.size(); ++i) {
-                if (tempInput == contact[i].getName()) {
+                if (tempInput == contact[i].getNumber() ||
+                    tempInput == contact[i].getName()) {
                     return i;
                 }
             }
         }
         std::cout << "Contact with the same name or number was not found\n";
         return ERROR;
-    }
 
+    }
 public:
-    void add() {
-        Contact temp;
-        std::string tempNumber;
-        std::string tempName;
-        do {
-            std::cout << "Enter contact name:";
-            std::cin >> tempName;
-            temp.setName(tempName);
-            std::cout << "Enter the " << tempName << "'s phone number:";
-            std::cin >> tempNumber;
-            temp.setNumber(tempNumber);
 
-            if (Contact::validationNumber(tempNumber)) {
-                contact.push_back(temp);
-                return;
-            } else std::cout << "The phone number was entered incorrectly! Try again!\n";
-        } while (true);
+    void add(std::string &name, std::string &number)
+    {
+        Contact temp;
+        temp.setName(name);
+        temp.setNumber(number);
+        if (Contact::validationNumber(number)) contact.push_back(temp);
+        else std::cout << "The phone number was entered incorrectly! Try again!\n";
     }
 
-    void call(){
-        int tempCall = contactRequest();
+    void call(std::string &tempInput)
+    {
+        int tempCall = contactRequest(tempInput);
         if (tempCall != ERROR){
             std::cout << "*****CALL!!!*****\n";
             std::cout << contact[tempCall].getNumber() << std::endl;
         }
     }
 
-    void sms(){
-        int tempSms = contactRequest();
+    void sms(std::string &tempInput){
+        int tempSms = contactRequest(tempInput);
         std::string message;
         if (tempSms != ERROR){
             std::cout << "Enter SMS message: ";
@@ -107,9 +91,27 @@ int main() {
     while (true){
         std::cout << "Enter the command add, call, sms or exit: ";
         std::cin >> command;
-        if (command == "add") telephone.add();
-        else if (command == "call") telephone.call();
-        else if (command == "sms") telephone.sms();
+        if (command == "add"){
+            std::cout << "Enter contact name:";
+            std::string tempName;
+            std::cin >> tempName;
+            std::cout << "Enter the " << tempName << "'s phone number:";
+            std::string tempNumber;
+            std::cin >> tempNumber;
+            telephone.add(tempName, tempNumber);
+        }
+        else if (command == "call") {
+            std::string tempInput;
+            std::cout << "Enter the name or number contact: ";
+            std::cin >> tempInput;
+            telephone.call(tempInput);
+        }
+        else if (command == "sms") {
+            std::string tempInput;
+            std::cout << "Enter the name or number contact: ";
+            std::cin >> tempInput;
+            telephone.sms(tempInput);
+        }
         else if (command == "exit") return 0;
         else std::cout << "Input is not correct! Try again!!!\n";
     }
